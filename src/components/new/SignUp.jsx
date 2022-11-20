8837cac4c2b0359817844116ccbf7573a715e20c
Emmanuel Kpendo <emmanuelkpendo1@gmail.com>
2022-11-20 21:19:04 +0000

    Added vector image to sign up field

--- src/components/new/SignUp.jsx
+++ src/components/new/SignUp.jsx
@@ -1,5 +1,6 @@
 import {
   Avatar,
+  Box,
   Button,
   Checkbox,
   FormControl,
@@ -21,6 +22,7 @@ import LockIcon from "@mui/icons-material/Lock";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import VectorImg from "../../assets/auth.svg";
 
 // Username Style
 const UsernameField = styled(TextField)({
@@ -84,199 +86,219 @@ const SignUp = () => {
   return (
     <Paper
       elevation={3}
+      component="section"
       sx={{
+        display: "grid",
+        gridTemplateColumns: {
+          xs: "1fr",
+          md: "1fr 1fr",
+        },
+        // background: "red",
+        gap: "3rem",
+        width: "max-content",
         height: "max-content",
-        width: "20rem",
         margin: "1rem auto",
-        padding: "1rem",
-        display: "flex",
-        flexDirection: "column",
-        gap: "0.5rem",
       }}
     >
-      <Avatar sx={{ bgcolor: deepOrange[500], marginInline: "auto" }}>
-        <AddCircleOutlineIcon />
-      </Avatar>
-      <Typography
-        variant="h5"
-        sx={{ textAlign: "center", fontWeight: "600", color: "#5a5a5a" }}
-      >
-        Sign Up
-      </Typography>
-      <Typography
-        variant="h6"
+      <Box
         sx={{
-          textAlign: "center",
-          fontWeight: "300",
-          color: "#5a5a5a",
-          fontSize: "1rem",
-          mb: 1,
+          width: "22rem",
+          height: "20rem",
+          display: { xs: "none", md: "flex" },
+          margin: "auto",
+          padding: "0 1rem",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        Create a React Account
-      </Typography>
-      <form style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
-        <UsernameField
-          id="name"
-          label="Name"
-          variant="standard"
-          fullWidth
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <PersonOutlineIcon />
-              </InputAdornment>
-            ),
-          }}
-          required
+        <img
+          src={VectorImg}
+          alt={VectorImg}
+          style={{ width: "100%", height: "auto" }}
         />
-        <EmailField
-          id="email"
-          label="Email"
-          variant="standard"
-          type="email"
-          fullWidth
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <PersonOutlineIcon />
-              </InputAdornment>
-            ),
-          }}
-          required
-        />
-        <FormControl required>
-          <FormLabel
-            id="radio-buttons-group-label"
-            sx={{ color: deepOrange[500] }}
-          >
-            Gender
-          </FormLabel>
-          <RadioGroup
-            aria-labelledby="radio-buttons-group-label"
-            defaultValue="male"
-            name="radio-buttons-group"
-            row
-          >
-            <FormControlLabel
-              value="male"
-              control={
-                <Radio
-                  sx={{
-                    color: deepOrange[500],
-                    "&.Mui-checked": { color: deepOrange[500] },
-                  }}
-                />
-              }
-              label="Male"
-            />
-            <FormControlLabel
-              value="female"
-              control={
-                <Radio
-                  sx={{
-                    color: deepOrange[500],
-                    "&.Mui-checked": { color: deepOrange[500] },
-                  }}
-                />
-              }
-              label="Female"
-            />
-            <FormControlLabel
-              value="other"
-              control={
-                <Radio
-                  sx={{
-                    color: deepOrange[500],
-                    "&.Mui-checked": { color: deepOrange[500] },
-                  }}
-                />
-              }
-              label="Other"
-            />
-          </RadioGroup>
-        </FormControl>
-        <PasswordField
-          id="password"
-          label="Password"
-          variant="standard"
-          type={view ? "text" : "password"}
-          fullWidth
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <LockIcon />
-              </InputAdornment>
-            ),
-            endAdornment: (
-              <InputAdornment
-                position="end"
-                onClick={handleView}
-                sx={{ cursor: "default" }}
-              >
-                {view ? <VisibilityOffIcon /> : <VisibilityIcon />}
-              </InputAdornment>
-            ),
-          }}
-          required
-        />
-        <PasswordField
-          id="confirm-password"
-          label="Confirm Password"
-          variant="standard"
-          type={confirmView ? "text" : "password"}
-          fullWidth
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <LockIcon />
-              </InputAdornment>
-            ),
-            endAdornment: (
-              <InputAdornment
-                position="end"
-                onClick={handleConfirmView}
-                sx={{ cursor: "default" }}
-              >
-                {confirmView ? <VisibilityOffIcon /> : <VisibilityIcon />}
-              </InputAdornment>
-            ),
-          }}
-          required
-        />
-
-        <FormControlLabel
-          control={
-            <Checkbox
-              size="small"
-              sx={{
-                color: deepOrange[500],
-                "&.Mui-checked": { color: deepOrange[500] },
-              }}
-            />
-          }
-          label="I accept the terms and conditions"
-        />
-        <Button
-          variant="contained"
+      </Box>
+      <Box
+        sx={{
+          height: "max-content",
+          width: "20rem",
+          padding: "1rem",
+          display: "flex",
+          flexDirection: "column",
+          gap: "0.5rem",
+        }}
+      >
+        <Avatar sx={{ bgcolor: deepOrange[500], marginInline: "auto" }}>
+          <AddCircleOutlineIcon />
+        </Avatar>
+        <Typography
+          variant="h5"
+          sx={{ textAlign: "center", fontWeight: "600", color: "#5a5a5a" }}
+        >
+          Sign Up
+        </Typography>
+        <Typography
+          variant="h6"
           sx={{
-            borderRadius: "3rem",
-            bgcolor: deepOrange[500],
-            "&:hover": {
-              bgcolor: deepOrange[800],
-            },
+            textAlign: "center",
+            fontWeight: "300",
+            color: "#5a5a5a",
+            fontSize: "1rem",
+            mb: 1,
           }}
         >
-          Submit
-        </Button>
-      </form>
+          Create a React Account
+        </Typography>
+        <form style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+          <UsernameField
+            id="name"
+            label="Name"
+            variant="standard"
+            fullWidth
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <PersonOutlineIcon />
+                </InputAdornment>
+              ),
+            }}
+            required
+          />
+          <EmailField
+            id="email"
+            label="Email"
+            variant="standard"
+            type="email"
+            fullWidth
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <PersonOutlineIcon />
+                </InputAdornment>
+              ),
+            }}
+            required
+          />
+          <FormControl required>
+            <FormLabel
+              id="radio-buttons-group-label"
+              sx={{ color: deepOrange[500] }}
+            >
+              Gender
+            </FormLabel>
+            <RadioGroup
+              aria-labelledby="radio-buttons-group-label"
+              defaultValue="male"
+              name="radio-buttons-group"
+              row
+            >
+              <FormControlLabel
+                value="male"
+                control={
+                  <Radio
+                    sx={{
+                      color: deepOrange[500],
+                      "&.Mui-checked": { color: deepOrange[500] },
+                    }}
+                  />
+                }
+                label="Male"
+              />
+              <FormControlLabel
+                value="female"
+                control={
+                  <Radio
+                    sx={{
+                      color: deepOrange[500],
+                      "&.Mui-checked": { color: deepOrange[500] },
+                    }}
+                  />
+                }
+                label="Female"
+              />
+            </RadioGroup>
+          </FormControl>
+          <PasswordField
+            id="password"
+            label="Password"
+            variant="standard"
+            type={view ? "text" : "password"}
+            fullWidth
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <LockIcon />
+                </InputAdornment>
+              ),
+              endAdornment: (
+                <InputAdornment
+                  position="end"
+                  onClick={handleView}
+                  sx={{ cursor: "default" }}
+                >
+                  {view ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                </InputAdornment>
+              ),
+            }}
+            required
+          />
+          <PasswordField
+            id="confirm-password"
+            label="Confirm Password"
+            variant="standard"
+            type={confirmView ? "text" : "password"}
+            fullWidth
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <LockIcon />
+                </InputAdornment>
+              ),
+              endAdornment: (
+                <InputAdornment
+                  position="end"
+                  onClick={handleConfirmView}
+                  sx={{ cursor: "default" }}
+                >
+                  {confirmView ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                </InputAdornment>
+              ),
+            }}
+            required
+          />
 
-      <div style={{ display: "flex", gap: "0.5rem" }}>
-        <Typography variant="span">Already having an account?</Typography>
-        <Link href="#" underline="hover" color={deepOrange[800]}>
-          Sign In
-        </Link>
-      </div>
+          <FormControlLabel
+            control={
+              <Checkbox
+                size="small"
+                sx={{
+                  color: deepOrange[500],
+                  "&.Mui-checked": { color: deepOrange[500] },
+                }}
+              />
+            }
+            label="I accept the terms and conditions"
+          />
+          <Button
+            variant="contained"
+            sx={{
+              borderRadius: "3rem",
+              bgcolor: deepOrange[500],
+              "&:hover": {
+                bgcolor: deepOrange[800],
+              },
+            }}
+          >
+            Submit
+          </Button>
+        </form>
+
+        <div style={{ display: "flex", gap: "0.5rem" }}>
+          <Typography variant="span">Already having an account?</Typography>
+          <Link href="#" underline="hover" color={deepOrange[800]}>
+            Sign In
+          </Link>
+        </div>
+      </Box>
     </Paper>
   );
 };
