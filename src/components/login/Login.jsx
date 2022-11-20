cde5327598e30f7c69ed38f32d4e0e4e2bcb778c
Emmanuel Kpendo <emmanuelkpendo1@gmail.com>
2022-11-20 16:53:47 +0000

    Done with Login Form Design

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,134 @@
+import {
+  Avatar,
+  Button,
+  InputAdornment,
+  Paper,
+  styled,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import LockOpenIcon from "@mui/icons-material/LockOpen";
+import { deepOrange } from "@mui/material/colors";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import LockIcon from "@mui/icons-material/Lock";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+
+// Username Style
+const UsernameField = styled(TextField)({
+  "& label.Mui-focused": {
+    color: deepOrange[500],
+  },
+  "& .MuiInput-underline:after": {
+    borderBottomColor: deepOrange[500],
+  },
+  "&.Mui-focused fieldset": {
+    borderColor: deepOrange[500],
+  },
+});
+
+// Password Style
+const PasswordField = styled(TextField)({
+  "& label.Mui-focused": {
+    color: deepOrange[500],
+  },
+  "& .MuiInput-underline:after": {
+    borderBottomColor: deepOrange[500],
+  },
+  "&.Mui-focused fieldset": {
+    borderColor: deepOrange[500],
+  },
+});
+
+const Login = () => {
+  // View/ hide password
+  const [view, setView] = useState(false);
+  const handleView = () => {
+    if (view === false) {
+      setView(true);
+    } else {
+      setView(false);
+    }
+  };
+
+  return (
+    <Paper
+      elevation={3}
+      sx={{
+        height: "30rem",
+        width: "23rem",
+        margin: "3rem auto",
+        padding: "2rem",
+        display: "flex",
+        flexDirection: "column",
+        gap: "1rem",
+      }}
+    >
+      <Avatar sx={{ bgcolor: deepOrange[500], marginInline: "auto" }}>
+        <LockOpenIcon />
+      </Avatar>
+      <Typography
+        variant="h5"
+        sx={{ textAlign: "center", fontWeight: "600", color: "#5a5a5a" }}
+      >
+        Login
+      </Typography>
+      <form style={{ display: "flex", flexDirection: "column", gap: "2rem" }}>
+        <UsernameField
+          id="username"
+          label="Username"
+          variant="standard"
+          fullWidth
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <PersonOutlineIcon />
+              </InputAdornment>
+            ),
+          }}
+          required
+        />
+        <PasswordField
+          id="username"
+          label="Password"
+          variant="standard"
+          type={view ? "text" : "password"}
+          fullWidth
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <LockIcon />
+              </InputAdornment>
+            ),
+            endAdornment: (
+              <InputAdornment
+                position="end"
+                onClick={handleView}
+                sx={{ cursor: "default" }}
+              >
+                {view ? <VisibilityOffIcon /> : <VisibilityIcon />}
+              </InputAdornment>
+            ),
+          }}
+          required
+        />
+        <Button
+          variant="contained"
+          sx={{
+            borderRadius: "3rem",
+            marginTop: "1rem",
+            bgcolor: deepOrange[500],
+            "&:hover": {
+              bgcolor: deepOrange[800],
+            },
+          }}
+        >
+          Submit
+        </Button>
+      </form>
+    </Paper>
+  );
+};
+
+export default Login;
